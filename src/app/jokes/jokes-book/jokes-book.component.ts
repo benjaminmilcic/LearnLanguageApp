cfddd008a3907bc5680195b659ae9755e8c3694d
Benjamin Milčić <you@example.com>
2022-09-27 12:18:28 +0200

    v0.16 work on book section

--- src/app/jokes/jokes-book/jokes-book.component.ts
+++ src/app/jokes/jokes-book/jokes-book.component.ts
@@ -1,30 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-jokes-book',
   templateUrl: './jokes-book.component.html',
   styleUrls: ['./jokes-book.component.css']
 })
-export class JokesBookComponent implements OnInit {
+export class JokesBookComponent implements AfterViewInit {
+
+  @ViewChild('bookFront') bookFront: ElementRef;
+  @ViewChild('bookBack') bookBack: ElementRef;
+  @ViewChild('bookWrap') bookWrap: ElementRef;
 
   constructor() { }
 
-  ngOnInit() {
+  ngAfterViewInit(): void {
+    const bookFrontElement=this.bookFront.nativeElement
+    bookFrontElement.addEventListener('mouseenter', () => {
+      this.bookWrap.nativeElement.classList.add('rotate');
+    });
+    bookFrontElement.addEventListener('mouseleave', () => {
+      this.bookWrap.nativeElement.classList.remove('rotate');
+    });
+    bookFrontElement.addEventListener('click', () => {
+      this.bookWrap.nativeElement.classList.add('flip');
+    });
+    this.bookBack.nativeElement.addEventListener('click', () => {
+      this.bookWrap.nativeElement.classList.remove('flip');
+    });
   }
-
-//   $(".book").mouseenter(function() {
-//     $(this).parent(".book-wrap").addClass("rotate");
-//   });
-
-//   $(".book").mouseleave(function() {
-//     $(this).parent(".book-wrap").removeClass("rotate");
-//   });
-
-//   $(".book").click(function() {
-//     $(this).parent(".book-wrap").addClass("flip");
-//   });
-
-//   $(".book-back").click(function() {
-//     $(this).parent(".book-wrap").removeClass("flip");
-//   });
 }
